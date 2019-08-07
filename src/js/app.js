7b365417998920857cd64266f2d56c5f92b5fca4
crc-32 <thomas.c.england@gmail.com>
2019-08-07 23:46:40 +0100

    Initial Remi commit

--- src/js/app.js
+++ src/js/app.js
@@ -5,84 +5,75 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
+var Voice = require('ui/voice');
+var Vibe = require('ui/vibe');
+var Settings = require('settings');
+var ajax = require('ajax');
+
+const statusDef = {separator: 'none', color: 'white', backgroundColor: 'black'};
+
+Settings.config(
+  { url: 'https://assist.crc32.dev/config.html' },
+  function(e) {
+    console.log('opening configurable');
+  },
+  function(e) {
+    console.log('closed configurable');
+  }
+);
+
+function dictate()
+{
+  Voice.dictate('start', true, function(e) {
+    if (e.err) {
+      var card = new UI.Card({
+        title: 'Error',
+        body: data,
+        scrollable: true,
+        status: statusDef
+      });
+      card.show();
+    }
+
+    ajax({
+        url: encodeURI(`https://assist.crc32.dev/assist/?query=${e.transcription}&clientid=${Settings.option('clientid')}&clientsecret=${Settings.option('clientsecret')}&clientrefresh=${Settings.option('clientrefresh')}`),
+        method: 'GET',
+        type: undefined,
+        headers: {
+        }
+      },
+      function(data, status, request) {
+        var card = new UI.Card({
+          title: 'Assistant',
+          body: data,
+          scrollable: true,
+          status: statusDef,
+          style: 'large'
+        });
+        card.on('click', 'select', dictate);
+        Vibe.vibrate('short');
+        card.show();
+      },
+      function(error, status, request) {
+        var card = new UI.Card({
+          title: 'Error',
+          body: 'Error occured while processing, check your internet!\n' + status,
+          scrollable: true,
+          status: statusDef
+        });
+        Vibe.vibrate('short');
+        card.show();
+      }
+    );
+
+  });
+}
 
 var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
+  title: 'Remi',
+  body: 'Press select to dictate',
+  status: statusDef
 });
 
+main.on('click', 'select', dictate);
 main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }, {
-        title: 'Third Item',
-      }, {
-        title: 'Fourth Item',
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    backgroundColor: 'black'
-  });
-  var radial = new UI.Radial({
-    size: new Vector2(140, 140),
-    angle: 0,
-    angle2: 300,
-    radius: 20,
-    backgroundColor: 'cyan',
-    borderColor: 'celeste',
-    borderWidth: 1,
-  });
-  var textfield = new UI.Text({
-    size: new Vector2(140, 60),
-    font: 'gothic-24-bold',
-    text: 'Dynamic\nWindow',
-    textAlign: 'center'
-  });
-  var windSize = wind.size();
-  // Center the radial in the window
-  var radialPos = radial.position()
-      .addSelf(windSize)
-      .subSelf(radial.size())
-      .multiplyScalar(0.5);
-  radial.position(radialPos);
-  // Center the textfield in the window
-  var textfieldPos = textfield.position()
-      .addSelf(windSize)
-      .subSelf(textfield.size())
-      .multiplyScalar(0.5);
-  textfield.position(textfieldPos);
-  wind.add(radial);
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
